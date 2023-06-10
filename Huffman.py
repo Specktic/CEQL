bba93de4d2b27edaaf1c4839c797c6db49ff5961
Specktic <111095859+Specktic@users.noreply.github.com>
2023-06-09 21:25:29 -0600

    Add tree_maker method to Huffman.py

--- Huffman.py
+++ Huffman.py
@@ -15,3 +15,14 @@ def huffman_tree(node, string=''):
     d.update(huffman_tree(l, string + '0'))
     d.update(huffman_tree(r, string + '1'))
     return d
+
+
+def tree_maker(nodes):
+    while len(nodes) > 1:
+        (key1, c1) = nodes[-1]
+        (key2, c2) = nodes[-2]
+        nodes = nodes[:-2]
+        node = Node(key1, key2)
+        nodes.append((node, c1 + c2))
+        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+    return nodes[0][0]
