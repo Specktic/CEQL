f4002a6ac19b763262cb26c45f470622566b634b
Specktic <111095859+Specktic@users.noreply.github.com>
2023-06-09 21:17:46 -0600

    Add Node class to Huffman.py for new implementation

--- Huffman.py
+++ Huffman.py
@@ -1,1 +1,7 @@
+class Node(object):
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
 
+    def children(self):
+        return self.left, self.right
